a90aa9f430f73f08e10873eda35bfc4fd120723c
Cristian Paiva <38576658+crisLeo-P01@users.noreply.github.com>
2024-08-22 12:28:16 -0300

    Update | Funcionalidad. falta adaptar el nav en mobile

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,8 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.querySelector("#menu-toggle");
-  const menu = document.querySelector("#mobile-menu");
+// FUNCIONAMINENTO ENTRE CAMBIO DE RESOLUCION MOBILE/DESKTOP
+const menuToggle = document.querySelector("#menu-toggle");
+const menu = document.querySelector("#mobile-menu");
 
-  menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-  });
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
+
+// FUNCIONAMIENTO DE LOS LINKS
+const navLinks = document.querySelectorAll(".nav-link");
+
+function setActiveLink() {
+  const scrollPosition = window.scrollY + 100; // Ajusta según la altura del header
+
+  navLinks.forEach((link) => {
+    const section = document.querySelector(link.getAttribute("href"));
+    const offsetTop = section.offsetTop;
+    const offsetHeight = section.offsetHeight;
+
+    if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
+      link.classList.add("bg-orange-600", "text-white");
+    } else {
+      link.classList.remove("bg-orange-600", "text-white");
+    }
+  });
+}
+
+setActiveLink();
+window.addEventListener("scroll", setActiveLink);
